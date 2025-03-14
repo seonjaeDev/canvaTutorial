4b0e68bbcd2238e7c5d2a5fdec3a0ee0b0e02ba4
seonjaeDev <pfsn.valdonjefferson@outlook.com>
2025-03-14 20:42:03 +0800

    added let in JavaScript, change section font size

--- canvaWebsite.js
+++ canvaWebsite.js
@@ -6,8 +6,9 @@ document.getElementById("c-palettes").style.display = "none";
 // web content
 document.getElementById("webTitle").innerHTML = "Canva Tutorial";
 
-document.getElementById("pageTitle").innerHTML = "Canva";
-document.getElementById("pageTitle").style.fontFamily = "playwrite-hu, sans-serif";
+let headerTitleElement = document.getElementById("pageTitle");
+headerTitleElement.innerHTML = "Canva";
+headerTitleElement.style.fontFamily = "playwrite-hu, sans-serif";
 document.getElementById("pageSubHeader").innerHTML = "A Creative Tool; Enhancing Editing Skills of Senior High School Student";
 
 document.getElementById("navButton1").style.backgroundColor = "#FF5252";
